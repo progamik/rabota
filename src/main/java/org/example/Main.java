c1a3071102b76f3db255a5e16ba9f2332976c797
Nikita <nikita_smatievich@bk.ru>
2025-04-30 16:53:50 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(calc(sc.nextLine()));
+    }
+
+    public static String calc(String input) {
+        String[] parts = input.trim().split("");
+        if (parts.length != 3) throw new RuntimeException(" ");
+
+        int a = parseInt(parts[0]);
+        int b = parseInt(parts[2]);
+        if (a < 1 || a > 10 || b < 1 || b > 10) throw new RuntimeException("Числа должны быть от 1 до 10");
+
+        String op = parts[1];
+        int result = switch (op) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            case "/" -> a / b;
+            default -> throw new RuntimeException("Недопустимая операция");
+        };
+        return String.valueOf(result);
+    }
+
+    static int parseInt(String s) {
+        try {
+            return Integer.parseInt(s);
+        } catch (Exception e) {
+            throw new RuntimeException("Вводите только целые числа");
+        }
+    }
+}
+
